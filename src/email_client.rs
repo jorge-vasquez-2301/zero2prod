ea4441b5f8982aee70c193c539ec9fefe30ce6cf
Jorge Vasquez <jv2301@gmail.com>
2023-06-26 16:16:49 -0400

    Format

--- src/email_client.rs
+++ src/email_client.rs
@@ -37,7 +37,7 @@ impl EmailClient {
         let request_body = SendEmailRequest {
             from: self.sender.as_ref(),
             to: recipient.as_ref(),
-            subject: subject,
+            subject,
             html_body: html_content,
             text_body: text_content,
         };
