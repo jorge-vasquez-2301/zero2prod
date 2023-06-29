b66505015bc6c0506b2cb404c7f0790fcd2205f9
Jorge Vasquez <jv2301@gmail.com>
2023-06-29 11:40:53 -0400

    Move password hashing to the blocking thread pool

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,3 +1,4 @@
+use tokio::task::JoinHandle;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
@@ -40,4 +41,13 @@ where
 pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     LogTracer::init().expect("Failed to set logger");
     tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    let current_span = tracing::Span::current();
+    tokio::task::spawn_blocking(move || current_span.in_scope(f))
 }
