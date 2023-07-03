b2d4b33b16d1f06759df36ef62c1d2c56ce6f708
Jorge Vasquez <jv2301@gmail.com>
2023-07-03 16:02:13 -0400

    Add seed user

---
+++ migrations/20230703195601_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'ddf8994f-d522-4659-8d02-c1d479057be6',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$TKJCSc3sArWrbq3tWD7ADw$M01ICC7plg1LQVQk2/TmKiPIb0143w0xh6SxLhJaYJ8'
+);
